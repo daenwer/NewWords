e84c7ac1c2239a99f7a903aa010196ab1a9c985c
dzmitry <daenwer@gmail.com>
2022-09-19 16:33:58 +0300

    added static

--- NewWords/settings.py
+++ NewWords/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'app', 'media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'app', 'static')
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- NewWords/urls.py
+++ NewWords/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+from NewWords import settings
 from app.views import TokenVerificationView, ForTestView
 
 # app_name = "app"
@@ -28,3 +32,6 @@ urlpatterns = [
     # path('admin/', site.urls),
     path('', ForTestView.as_view()),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
