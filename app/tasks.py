33b7ccf3a1269b2ac03865998c99f00487226971
dzmitry <daenwer@gmail.com>
2022-09-21 14:29:40 +0300

    added random repeat time for second repeat

--- app/tasks.py
+++ app/tasks.py
@@ -58,30 +58,38 @@ def create_new_celery_task(task_id):
         minute=task.user.user_schedule.finish_time.minute
     )
 
+    start_hour = task.user.user_schedule.start_time.hour
+    start_minute = task.user.user_schedule.start_time.minute
+
     if next_datetime_repeat <= lately_today:
-        hour = next_datetime_repeat.hour
-        minute = next_datetime_repeat.minute
-    else:
-        if next_step_repeat == 1:
-            start_hour = task.user.user_schedule.start_time.hour
-            start_minute = task.user.user_schedule.start_time.minute
+        if next_datetime_repeat.hour <= task.user.user_schedule.start_time.hour:
             start = start_hour + start_minute / 60
             finish = start + 2
-            alarm = random.uniform(start, finish)
-            hour = int(alarm)
-            minute = int((alarm - hour) * 60)
+        else:
+            if next_datetime_repeat.hour - start_hour > 1:
+                start = (
+                    next_datetime_repeat.hour - 1 +
+                    next_datetime_repeat.minute / 60
+                )
+                finish = start + 1
+            else:
+                start = start_hour + start_minute / 60
+                finish = start + 1
+    else:
+        if next_step_repeat == 1:
+            start = start_hour + start_minute / 60
+            finish = start + 2
         else:
             if next_datetime_repeat.date() == lately_today.date():
                 next_datetime_repeat += datetime.timedelta(days=1)
-            start_hour = task.user.user_schedule.start_time.hour
-            start_minute = task.user.user_schedule.start_time.minute
             finish_hour = task.user.user_schedule.finish_time.hour - 1
             finish_minute = task.user.user_schedule.finish_time.minute
             start = start_hour + start_minute / 60
             finish = finish_hour + finish_minute / 60
-            alarm = random.uniform(start, finish)
-            hour = int(alarm)
-            minute = int((alarm - hour) * 60)
+
+    alarm = random.uniform(start, finish)
+    hour = int(alarm)
+    minute = int((alarm - hour) * 60)
 
     user_next_datetime_repeat = datetime.datetime(
         year=next_datetime_repeat.year,
