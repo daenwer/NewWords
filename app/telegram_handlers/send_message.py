6ba3b175fe3385fe3400bd85dfb36d58c02fd25b
dzmitry <daenwer@gmail.com>
2022-09-27 17:30:42 +0300

    added try - except

--- app/telegram_handlers/send_message.py
+++ app/telegram_handlers/send_message.py
@@ -4,19 +4,24 @@ from aiogram import Bot
 from asgiref.sync import async_to_sync
 
 from NewWords.settings import TELEGRAM_TOKEN, BASE_DIR
+from app.telegram_handlers.sync_async import _get_user
 
 
 @async_to_sync
 async def send_message(channel_id: int, text: str, audio_path: str):
     bot = Bot(token=TELEGRAM_TOKEN)
-
-    if audio_path:
-        path = os.path.join(BASE_DIR, 'app', 'static', 'audio', audio_path)
-        await bot.send_voice(
-            chat_id=channel_id, voice=open(path, 'rb'), caption=text
-        )
-    else:
-        await bot.send_message(chat_id=channel_id, text=text)
-
+    # TODO: переписать на конкретные ошибки
+    try:
+        if audio_path:
+            path = os.path.join(BASE_DIR, 'app', 'static', 'audio', audio_path)
+            await bot.send_voice(
+                chat_id=channel_id, voice=open(path, 'rb'), caption=text
+            )
+        else:
+            await bot.send_message(chat_id=channel_id, text=text)
+    except:
+        user = await _get_user()
+        user.is_active = False
+        user.save()
     session = await bot.get_session()
     await session.close()
